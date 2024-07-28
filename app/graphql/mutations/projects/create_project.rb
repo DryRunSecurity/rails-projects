a1182ecb6671be6ce62c25e2059242873df7d275
jordan-dr <126520221+jordan-dr@users.noreply.github.com>
2024-07-28 17:40:31 -0500

    Update create_project.rb

--- app/graphql/mutations/projects/create_project.rb
+++ app/graphql/mutations/projects/create_project.rb
@@ -5,14 +5,13 @@ module Mutations
 
       argument :name, String, required: true
       argument :description, String, required: false
+      argument :user_id, ID, required: true
 
       field :project, Types::ProjectType, null: false
       field :errors, [String], null: false
 
-      def resolve(name:, description: nil)
-        user = context[:current_user]
-        project = Project.new(name: name, description: description, user_id: user.id)
-
+      def resolve(name:, description: nil, user_id:)
+        project = Project.new(name: name, description: description, user_id: user_id)
         if project.save
           {
             project: project,
