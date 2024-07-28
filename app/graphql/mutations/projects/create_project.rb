15a0bd43b6b8e95d9794d126b769f621c77031cf
jordan-dr <126520221+jordan-dr@users.noreply.github.com>
2024-07-28 15:53:05 -0500

    Update create_project.rb

--- app/graphql/mutations/projects/create_project.rb
+++ app/graphql/mutations/projects/create_project.rb
@@ -1,39 +1,37 @@
-# module Mutations
-#     module Projects
-#       class CreateProject < BaseMutation
-#         graphql_name 'CreateProject'
+module Mutations
+    module Projects
+      class CreateProject < BaseMutation
+        graphql_name 'CreateProject'
   
-#         argument :name, String, required: true
-#         argument :description, String, required: false
-#         argument :user_id, ID, required: true
+        argument :name, String, required: true
+        argument :description, String, required: false
+        argument :user_id, ID, required: true
   
-#         field :project, Types::ProjectType, null: false
-#         field :errors, [String], null: false
+        field :project, Types::ProjectType, null: false
+        field :errors, [String], null: false
+    
+        def resolve(name:, description: nil, user_id:)
+          project = Project.new(name: name, description: description, user_id: user_id)
+          if project.save
+            {
+              project: project,
+              errors: []
+            }
+          else
+            {
+              project: nil,
+              errors: project.errors.full_messages
+            }
+          end
+        end
   
-#         authorize :create, Project
+        private
   
-#         def resolve(name:, description: nil, user_id:)
-#           project = Project.new(name: name, description: description, user_id: user_id)
-#           if project.save
-#             {
-#               project: project,
-#               errors: []
-#             }
-#           else
-#             {
-#               project: nil,
-#               errors: project.errors.full_messages
-#             }
-#           end
-#         end
-  
-#         private
-  
-#         def authorized_find!(id)
-#           project = Project.find(id)
-#           authorize! :read, project
-#           project
-#         end
-#       end
-#     end
-#   end
+        def authorized_find!(id)
+          project = Project.find(id)
+          authorize! :read, project
+          project
+        end
+      end
+    end
+  end
